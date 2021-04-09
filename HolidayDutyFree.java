4bbacd019f70c7c09c1e6bccd4dfdb0927f01918
FABIAN EMMANUEL <fabulousval4ril@gmail.com>
2021-04-09 09:25:31 +0100

    Add files via upload

--- HolidayDutyFree.java
+++ HolidayDutyFree.java
@@ -10,14 +10,24 @@ All inputs will be integers. Please return an integer. Round down.
  */
 public class HolidayDutyFree {
     public static void main(String[] args) {
-        System.out.println(dutyFree(10, 10, 500));
+        System.out.println(dutyFree(10, 10, 500)); //500
+        System.out.println(dutyFree(12,50,1000)); //166
+        System.out.println(dutyFree(17,10,500));  //294
+        System.out.println(dutyFree(24,35,3000)); //357
+        System.out.println(dutyFree(1400,35,10000)); //20
+        System.out.println(dutyFree(700,26,7000)); //28
     }
 
     public static int dutyFree(int normPrice, int discount, int hol) {
+        return hol*100/(normPrice*discount);
+    }
+
+    /*
+        Solution 2
         double mainDiscount = discount * 0.01;
         double gainPerBottle = normPrice * mainDiscount;
         double ans = hol / gainPerBottle;
         return (int) ans;
-    }
+*/
 }
 
